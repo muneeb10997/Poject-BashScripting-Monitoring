934e9ba6b366f6eed4ce1c2555a4ce0a99dd9810
Muneeb <muneeburrehman10997@gmail.com>
2024-09-16 00:43:28 +0500

    final monitoring script

--- monitoring_script.sh
+++ monitoring_script.sh
@@ -5,14 +5,14 @@ date_time=$(date +"date = %Y/%m/%d_%H:%M:%S")
 
 
 # CPU Usage
-cpu_usage=$(top -b -n1 | grep "Cpu(s)" | awk '{printf "Total CPU usage is : %.1f%%", 100 - $8}')
+cpu_usage=$(top -b -n1 | grep "Cpu(s)" | awk '{printf "Total CPU usage is = %.1f%%", 100 - $8}')
 
 # Memory Usage
-memory_usage=$(top -b -n1 | grep "MiB Mem" | awk '{printf "Total memory used : %.1f%%", ($8/$4) * 100}')
+memory_usage=$(top -b -n1 | grep "MiB Mem" | awk '{printf "Total memory used = %.1f%%", ($8/$4) * 100}')
 
 
 #Disk Usage 
-disk_usage=$(df -h  | awk 'NR>1 {total_size+=$2; total_used+=$3} END {printf "Total disk usage : %.1f%%\n", (total_used / total_size) * 100}')
+disk_usage=$(df -h  | awk 'NR>1 {total_size+=$2; total_used+=$3} END {printf "Total disk usage = %.1f%%\n", (total_used / total_size) * 100}')
 
 
 # sending data to monitoring file
